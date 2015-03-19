1f9d71da1f4dd5dccd5eb075c6d57f092d2bae54
vipal25@yahoo.com <vicpal25@yahoo.com>
2015-03-19 14:33:14 -0700

    Users scaffolding model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render text: "hello, world!"
+    render text: "HOLA MUCHACHOS"
   end
+
+  def goodbye
+    render text: "adios"
+  end
+
 
 end
