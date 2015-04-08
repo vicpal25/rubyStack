13e32ad77f4600b7e94dac9774198d4857d333f4
vipal25@yahoo.com <vicpal25@yahoo.com>
2015-04-08 10:57:33 -0700

    User password / authentication validation integration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ActiveRecord::Base
-  has_many :microposts
-  validates :user, presence: true
-  validates :email, presence: true
+  before_save { self.email = email.downcase }
+  validates :name,  presence: true, length: { maximum: 50 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 },  format: { with: VALID_EMAIL_REGEX }
+    has_secure_password
+    validates :password, length: { minimum: 6 }
+    #     • The ability to save a securely hashed password_digest attribute to the database
+    # • A pair of virtual attributes18 (password and password_confirmation), including presence validations upon object creation and a validation requiring that they match
+    # 18In this context, virtual means that the attributes exist on the model object but do not correspond to columns in the database.
+    # • An authenticate method that returns the user when the password is correct (and false otherwise)
 end
